b02a0543a429f3df11cad6ecd410d6c0190d1e6d
unknown <islammoshiu@gmail.com>
2022-11-09 03:20:40 +0600

    firebase setup

--- index.js
+++ index.js
@@ -13,11 +13,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4rkfhhl.mongodb.net/?retryWrites=true&w=majority`;
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+try{
+ const serviceCollection = client.db("rocketShop").collection("services");
+
+ app.get("/services",async(req,res)=>{
+    const query={};
+    const cursor = serviceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send(services);
+ })
+}
+finally{
+
+}
+}
+run().catch(err=>console.error(err))
 
 
 app.get("/",(req,res)=>{
