eb7442733bb0456ca656b4f3210e74014b0785b3
unknown <islammoshiu@gmail.com>
2022-11-09 22:24:32 +0600

    delete method done

--- index.js
+++ index.js
@@ -32,11 +32,34 @@ try{
 });
 
 //review API
+app.get('/reviews', async (req, res) => {
+    let query = {};
+
+    if (req.query.email) {
+        query = {
+            email: req.query.email
+        }
+    }
+
+    const cursor = reviewCollection.find(query);
+    const reviews = await cursor.toArray();
+    res.send(reviews);
+});
+
+
 app.post('/reviews', async (req, res) => {
     const review = req.body;
     const result = await reviewCollection.insertOne(review);
     res.send(result);
 });
+
+
+app.delete('/reviews/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await reviewCollection.deleteOne(query);
+    res.send(result);
+})
 
 }
 finally{
