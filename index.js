689a988426a61cb508f2df1d2af1dca71787b9ef
unknown <islammoshiu@gmail.com>
2022-11-09 15:21:40 +0600

    single query

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 //middle wares
@@ -23,6 +23,12 @@ try{
     const services = await cursor.toArray();
     res.send(services);
  })
+ app.get('/services/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const service = await serviceCollection.findOne(query);
+    res.send(service);
+});
 }
 finally{
 
